976fcf8baa7c3b3547cb79a7aaad23f1198de832
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2023-05-14 20:18:13 +0300

    BuyukKucukBulma
    Java ile klavyeden girilen N tane sayma sayısından en büyük ve en küçük sayıları bulan ve bu sayıları ekrana yazan program.

---
+++ BuyukKucukBulma/BuyukKucukBulma.java
@@ -0,0 +1,31 @@
+package Giris.BuyukKucukBulma;
+
+import java.util.Scanner;
+
+public class BuyukKucukBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int n = input.nextInt();
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + ". Sayıyı giriniz: ");
+            int num = input.nextInt();
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        System.out.println("En büyük sayı: " + max);
+        System.out.println("En küçük sayı: " + min);
+    }
+}
