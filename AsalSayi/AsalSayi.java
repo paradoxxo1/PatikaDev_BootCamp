64d44799ea5d07a3af37e95c639ed95a0b0edabc
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2023-05-14 20:24:35 +0300

    AsalSayi
    AsalSayi

---
+++ AsalSayi/AsalSayi.java
@@ -0,0 +1,21 @@
+package Giris.AsalSayi;
+
+
+public class AsalSayi {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asalMi = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+            if (asalMi) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
